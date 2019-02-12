7fd464d2db8b8d3f38b9e1e16ae0105ce3153011
unknown <t.goodacre@sakurasoft.co.jp>
2019-02-12 19:29:47 +0900

    added stylesheets, separated add and show list

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", homepage.Homepage)
-	http.HandleFunc("/todo_list", todo.EnterPage)
+	http.HandleFunc("/todo_list", todo.ViewList)
 	http.HandleFunc("/todo_list/add", todo.AddItem)
+
+	//Sreve static files with StripPrefix, in case the directory changes.
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+
 	log.Fatal(
 		http.ListenAndServe(":8080", nil),
 	)
--- todo_list/todo.go
+++ todo_list/todo.go
@@ -4,12 +4,16 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"sort"
 	"time"
 )
 
 type todoItem struct {
-	DateTime string
-	Notes    string
+	dateAdded        time.Time
+	dateTodo         time.Time
+	DisplayDateAdded string
+	DisplayTodoDate  string
+	Notes            string
 }
 
 //Variables for the main todo page
@@ -19,15 +23,18 @@ type Variables struct {
 
 var items []todoItem
 
-//EnterPage creates the main todo page, on navigation to the page
-func EnterPage(w http.ResponseWriter, r *http.Request) {
+//ViewList creates the main todo page, on navigation to the page
+func ViewList(w http.ResponseWriter, r *http.Request) {
+	//Sort the todo list by todo time.
+	_sortByTodoTime(items)
+
 	//Store the todo list in the PageVars struct sent to todo.html
 	PageVars := Variables{
 		TodoList: items,
 	}
 
 	//Parse the todo_list html
-	t, err := template.ParseFiles("todo_list/todo.html")
+	t, err := template.ParseFiles("todo_list/todo_list.html")
 	//Check for errors
 	if err != nil {
 		log.Println("template parsing error: ", err)
@@ -63,14 +70,23 @@ func AddItem(w http.ResponseWriter, r *http.Request) {
 		log.Println("date parsing error: ", err)
 	}
 
-	//Create a new todo, with a formatted date/time.
-	newTodo := todoItem{
-		DateTime: date.Format("02-Jan (Mon) 15:04"),
-		Notes:    r.Form.Get("notes"),
-	}
+	notes := r.Form.Get("notes")
 
-	//Insert the new todo into the start of the item list.
-	items = append([]todoItem{newTodo}, items...)
+	if err == nil && notes != "" {
+		//Create a new todo, with a formatted date/time.
+		newTodo := todoItem{
+			dateAdded:        time.Now(),
+			dateTodo:         date,
+			DisplayDateAdded: time.Now().Format("02-Jan (Mon) 15:04"),
+			DisplayTodoDate:  date.Format("02-Jan (Mon) 15:04"),
+			Notes:            notes,
+		}
+
+		//Insert the new todo into the start of the item list.
+		items = append([]todoItem{newTodo}, items...)
+
+		log.Println("todo item added")
+	}
 
 	//Store the todo list in the PageVars struct sent to todo.html
 	PageVars := Variables{
@@ -78,7 +94,7 @@ func AddItem(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Parse the todo_list html
-	t, err := template.ParseFiles("todo_list/todo.html")
+	t, err := template.ParseFiles("todo_list/todo_add.html")
 	//Check for parsing error
 	if err != nil {
 		log.Println("template parsing error: ", err)
@@ -90,4 +106,21 @@ func AddItem(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("template execute error: ", err)
 	}
+
+}
+
+func RemoveItem(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func _sortByTimeAdded(unsorted []todoItem) {
+	sort.Slice(unsorted, func(i, j int) bool {
+		return unsorted[i].dateAdded.Before(unsorted[j].dateAdded)
+	})
+}
+
+func _sortByTodoTime(unsorted []todoItem) {
+	sort.Slice(unsorted, func(i, j int) bool {
+		return unsorted[i].dateTodo.Before(unsorted[j].dateTodo)
+	})
 }
